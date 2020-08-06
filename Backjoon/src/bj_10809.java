d07f3d2851c4346a568e19a16a2a87c2c9d9ecd4
subin <soobin1080@naver.com>
2020-08-06 15:16:09 +0900

    알고리즘 공부_200806

---
+++ Backjoon/src/bj_10809.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class bj_10809 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		String s = sc.next();
+
+		int[] alphabet = new int[26];
+		char[] input = s.toCharArray();
+
+		for (int i = 0; i < input.length; i++) {
+			int idx = input[i] - 'a';
+			if (alphabet[idx] == 0) {
+				alphabet[idx] = i + 1;
+			}
+		}
+
+		for (int i = 0; i < alphabet.length; i++) {
+			System.out.print(alphabet[i] - 1 + " ");
+		}
+
+	}
+
+}
