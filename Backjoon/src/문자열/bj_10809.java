d110c749723fe6c28e15e0ad7aa0711955958def
subin <soobin1080@naver.com>
2020-08-25 19:35:48 +0900

    알고리즘공부_200825

---
+++ Backjoon/src/문자열/bj_10809.java
@@ -0,0 +1,31 @@
+package 문자열;
+
+import java.util.Scanner;
+
+public class bj_10809 {
+	
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sc = new Scanner(System.in);
+
+		String s = sc.next();
+
+		int[] alphabet = new int[26];
+		char[] input = s.toCharArray();
+
+		for (int i = 0; i < input.length; i++) {
+			int idx = input[i] - 'a';
+			if (alphabet[idx] == 0) {
+				alphabet[idx] = i + 1;
+			}
+		}
+
+		for (int i = 0; i < alphabet.length; i++) {
+			System.out.print(alphabet[i] - 1 + " ");
+		}
+
+	}
+
+}
