39a001917a1960588ee05600b86499f967ccefce
subin <soobin1080@naver.com>
2020-09-09 21:20:19 +0900

    알고리즘공부_200909

---
+++ Backjoon/src/bj_10870.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class bj_10870 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println(fibo(sc.nextInt()));
+	}
+
+	private static int fibo(int n) {
+		// TODO Auto-generated method stub
+		if(n==1)
+			return 1;
+		if(n==0)
+			return 0;
+		return fibo(n-1)+fibo(n-2);
+	}
+
+}
