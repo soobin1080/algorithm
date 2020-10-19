8522681eb4b799557fbf4b5c3d8f7d18f3fcc8e9
subin <soobin1080@naver.com>
2020-10-19 20:18:40 +0900

    알고리즘공부_201019

---
+++ Backjoon/src/bj_9095.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class bj_9095 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int TC = sc.nextInt();
+
+		int[] input = new int[TC];
+
+		int n = 0;
+		for (int i = 0; i < input.length; i++) {
+			input[i] = sc.nextInt();
+			n = (n < input[i]) ? input[i] : n;
+		}
+
+		int[] arr = new int[n + 1];
+
+		arr[0] = 0;
+		arr[1] = 1;
+		arr[2] = 2;
+		arr[3] = 4;
+
+		for (int i = 4; i < arr.length; i++) {
+			arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+		}
+
+		// 결과 리턴하기
+		for (int i = 0; i < input.length; i++) {
+			System.out.println(arr[input[i]]);
+		}
+
+	}
+
+}
---
+++ SWExpert/src/sw_1210.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class sw_1210 {
+
+	static boolean[][] visited;
+	static int[] dy = { 0, 0, -1 }; // 좌, 우, 상
+	static int[] dx = { -1, 1, 0 };
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		for (int TC = 0; TC < 10; TC++) {
+
+			int tc = sc.nextInt();
+
+			int map[][] = new int[100][100];
+
+			for (int i = 0; i < 100; i++) {
+				for (int j = 0; j < 100; j++) {
+					map[i][j] = sc.nextInt();
+				}
+			}
+
+			int x = 0, y = 0;
+			for (int i = 0; i < 100; i++) {
+				if (map[99][i] == 2) {
+					x = i;
+					y = 99;
+					break;
+				}
+			}
+
+			visited = new boolean[100][100];
+
+			while (y != 0) {
+
+				for (int i = 0; i < 3; i++) {
+					int ix = x + dx[i];
+					int iy = y + dy[i];
+
+					if (ix >= 0 && ix < 100 && iy >= 0 && iy < 100 && map[iy][ix] == 1 && !visited[iy][ix]) {
+						visited[iy][ix] = true;
+						y = iy;
+						x = ix;
+						break;
+					}
+				}
+			}
+
+			System.out.println("#" + tc + " " + x);
+
+		}
+
+	}
+
+}
