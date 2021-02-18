8a986d17d01515ec8ba4dd79a34905a67fb215c9
subin <soobin1080@naver.com>
2021-02-18 20:56:54 +0900

    210218_가운데를말해요

--- Backjoon/src/bj_1655_가운데를말해요.java
+++ Backjoon/src/bj_1655_가운데를말해요.java
@@ -1,28 +1,38 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class bj_1655_가운데를말해요 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
 
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		PriorityQueue<Integer> maxHeap = new PriorityQueue<>();
+		PriorityQueue<Integer> minHeap = new PriorityQueue<>((o1, o2) -> o2 - o1); // new Comparator 안쓰고 이렇게
+																					// 가능!!!!!!!대박스
 
 		int N = sc.nextInt();
+
 		for (int i = 0; i < N; i++) {
 			int n = sc.nextInt();
-			pq.add(n);
-			// Object[] arr = pq.toArray();
-			// System.out.println(Arrays.toString(arr));
-			/*
-			 * if (pq.size() % 2 == 0) if (pq.size() == 0) System.out.println(arr[0]); else
-			 * System.out.println(arr[pq.size() / 2 - 1]); else { if (pq.size() == 1)
-			 * System.out.println(arr[0]); else System.out.println(arr[pq.size() / 2 - 1]);
-			 * }
-			 */
-			System.out.println(pq.poll());
+
+			if (minHeap.size() == maxHeap.size()) {
+				minHeap.add(n);
+			} else
+				maxHeap.add(n);
+
+			if (!minHeap.isEmpty() && !maxHeap.isEmpty()) {
+				if (minHeap.peek() > maxHeap.peek()) {
+					maxHeap.add(minHeap.poll());
+					minHeap.add(maxHeap.poll());
+				}
+			}
+
+			sb.append(minHeap.peek() + "\n");
 		}
 
+		System.out.println(sb);
 		sc.close();
 	}
 
