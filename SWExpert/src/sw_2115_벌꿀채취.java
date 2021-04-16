bd9e0ab30cbe6ac9ec0f180c02e12264c0130633
subin <soobin1080@naver.com>
2021-04-16 20:17:09 +0900

    210416_벌꿀채취

--- SWExpert/src/sw_2115_벌꿀채취.java
+++ SWExpert/src/sw_2115_벌꿀채취.java
@@ -38,16 +38,16 @@ public class sw_2115_벌꿀채취 {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				selected = new boolean[N][N];
-				if (N - j > M) {
+				if (N - j >= M) {
 					for (int l = 0; l < M; l++) {
 						selected[i][j + l] = true;
 						honeyCapicity[0][l] = map[i][j + l];
 					}
 
 					// 두번째 사람
-					for (int a = 0; a < N; a++) {
+					for (int a = i; a < N; a++) {
 						a: for (int b = 0; b < N; b++) {
-							if (!selected[a][b] && N - b > M) {
+							if (!selected[a][b] && N - b >= M) {
 								// 첫번째 사람이랑 겹치는지 확인
 								for (int c = 0; c < M; c++) {
 									if (selected[a][b + c])
@@ -60,13 +60,13 @@ public class sw_2115_벌꿀채취 {
 								}
 
 								// 벌꿀채취
-								for (int k = 1; k < M; k++) {
+								for (int k = 1; k <= M; k++) {
 									used = new boolean[M];
 									saveMaxProfit(0, 0, k);
 								}
-
 								int cost = cost1 + cost2;
 								MAX = (MAX < cost) ? cost : MAX;
+								cost1=cost2=0;
 
 								for (int c = 0; c < M; c++) {
 									selected[a][b + c] = false;
@@ -93,19 +93,20 @@ public class sw_2115_벌꿀채취 {
 					p2 += honeyCapicity[1][i];
 				}
 			}
-			if (p1 <= C)
+			if (p1 <= C) {
 				for (int i = 0; i < M; i++) {
 					if (used[i]) {
 						c1 += (honeyCapicity[0][i] * honeyCapicity[0][i]);
 					}
 				}
-			if (p2 <= C)
+			}
+			if (p2 <= C) {
 				for (int i = 0; i < M; i++) {
 					if (used[i]) {
 						c2 += (honeyCapicity[1][i] * honeyCapicity[1][i]);
 					}
 				}
-
+			}
 			cost1 = (cost1 > c1) ? cost1 : c1;
 			cost2 = (cost2 > c2) ? cost2 : c2;
 			return;
