f4178972be200e1c419dd69d4be75dabc820574d
subin <soobin1080@naver.com>
2021-08-30 22:21:50 +0900

    210830_물놀이를가자

--- SWExpert/src/sw_10966_물놀이를가자.java
+++ SWExpert/src/sw_10966_물놀이를가자.java
@@ -1,15 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class sw_10966_물놀이를가자 {
 
 	static int[] dy = { -1, 1, 0, 0 }; // 상하좌우
 	static int[] dx = { 0, 0, -1, 1 };
-	static int distance_sum, distance_min;
-	static boolean visited[][];
-	static boolean find;
+	static int distance_sum;
+	static int distanceMap[][];
+	static Queue<Point> qu = new LinkedList<Point>();
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -26,57 +28,64 @@ public class sw_10966_물놀이를가자 {
 				map[i] = br.readLine().toCharArray();
 			}
 
-			/*
-			 * for (int i = 0; i < N; i++) { for (int j = 0; j < M; j++) {
-			 * System.out.print(map[i][j]); } System.out.println(); }
-			 * System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
-			 */
-
-			distance_sum = 0;
-
+			distanceMap = new int[N][M];
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < M; j++) {
-					if (map[i][j] == 'L') {
-						visited = new boolean[N][M];
-						visited[i][j] = true;
-						find = false;
-						distance_min = N + M;
-						System.out.println(i + "," + j + ": 시작");
-						dfs(N, M, i, j, map, 1);
-						distance_sum += distance_min;
+					if (map[i][j] == 'W') {
+						distanceMap[i][j]=-1;
+						qu.add(new Point(i, j));
 					}
 				}
 			}
+			bfs(N, M, map);
+
+			distance_sum = 0;
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < M; j++) {
+					if (map[i][j] == 'L') {
+						distance_sum += distanceMap[i][j];
+					}
+				}
+			}
+
 			System.out.println("#" + tc + " " + distance_sum);
 
 		}
 	}
 
-	private static void dfs(int N, int M, int y, int x, char[][] map, int d) {
+	private static void bfs(int N, int M, char[][] map) {
 
-		if (distance_min == 1)
-			return;
-		if (distance_min <= d)
-			return;
+		int distance = 0;
+		while (!qu.isEmpty()) {
+			distance++;
+			int size = qu.size();
+			for (int s = 0; s < size; s++) {
 
-		for (int i = 0; i < 4; i++) {
-			int ny = dy[i] + y;
-			int nx = dx[i] + x;
-			if (ny >= 0 && ny < N && nx >= 0 && nx < M && !visited[ny][nx]) {
-				System.out.println(ny + "," + nx);
-				if (map[ny][nx] == 'W') {
-					System.out.println("w찾음");
-					distance_min = (distance_min > d) ? d : distance_min;
-					System.out.println("distance_min:" + distance_min);
-					return;
-				} else {
-					visited[ny][nx] = true;
-					dfs(N, M, ny, nx, map, d + 1);
-					visited[ny][nx] = false;
+				Point now = qu.poll();
+
+				for (int i = 0; i < 4; i++) {
+					int ny = dy[i] + now.y;
+					int nx = dx[i] + now.x;
+					if (ny >= 0 && ny < N && nx >= 0 && nx < M) {
+						if (map[ny][nx] == 'L') {
+							if (distanceMap[ny][nx] == 0 || distanceMap[ny][nx] > distance) {
+								distanceMap[ny][nx] = distance;
+								qu.add(new Point(ny, nx));
+							}
+						}
+					}
 				}
 			}
-
 		}
-		return;
+	}
+
+	static class Point {
+		int y;
+		int x;
+
+		Point(int y, int x) {
+			this.y = y;
+			this.x = x;
+		}
 	}
 }
